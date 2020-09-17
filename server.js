a053e2807a2e2da9b8a269508cdb2fc6abdc859d
Bunyapon <j.iswithyou@gmail.com>
2020-09-17 22:46:32 +0700

    User Registered

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
-// connect Database
+const app = express();
 
+// connect Database
 connectDB();
 
-const app = express();
+//Init Middleware
+app.use(express.json({ extended: false}));
 
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
+//app.use('https path', require('file part'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
